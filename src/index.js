d55ed2a21324b1a9dd24dbde5007ca616b346981
cormojs <cormorant2+github@gmail.com>
2018-10-06 21:42:20 +0900

    tootsをmastodonから取得してconsoleに出力するデモを作った

--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
 import riot from 'riot'
 import myIndex from './tag/my-index.tag'
+import * as tootjs from 'tootjs'
+import axios from 'axios'
+
+var client = axios.create({
+  baseURL: 'http://localhost:8000',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  },
+  responseType: 'json'
+});
+
+var config = {
+  "access_token": "自分の入れてね",
+  "scope": "read write follow",
+  "host": "自分で入れてね"
+};
+
+var mastodon = new tootjs.Mastodon(config);
+
+mastodon.get('/timelines/home', {}).then(resp => {
+  for (let status of resp) {
+    console.log(status);
+  }
+});
 
 var createApp = function() {
   var app = {
     statuses: []
   }
-  var i = 0;
-  window.setInterval(function() {
-    app.statuses.push({
-      contents: `test${i}`,
-      hashtag: "#testTag",
-      account: {
-        display_name: 'tester',
-        screen_name: "@tester"
-      }
-    });
-    riot.update();
-    i++;
-  }, 10000)
 
   return app;
 };
